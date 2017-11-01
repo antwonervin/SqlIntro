9dafa59b1f7056b55fca8804adc76f8006b50073
Antwon Ervin <antwonervin@gmail.com>
2017-11-01 12:36:05 -0500

    Added Dapper Product Repository

---
+++ SqlIntro/DapperProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace SqlIntro
+{
+    public class DapperProductRepository
+    {
+        private readonly IDbConnection conn;
+
+        public DapperProductRepository(IDbConnection conn)
+        {
+            this.conn = conn;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return conn.Query<Product>("Select * from product");
+        }
+
+        public void DeleteProduct(int id)
+        {
+            conn.Execute("Delete from product where id =@id");
+        }
+
+        public void UpdateProduct(Product prod)
+        {
+            conn.Execute("Update product set name = @name where id=@id");
+        }
+
+        public void InsertProduct(Product prod)
+        {
+            conn.Execute("Insert into product (name) value (@name)", new {name = prod.Name});
+        }
+    }
+}
